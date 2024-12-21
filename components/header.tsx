3afa64c386a459f5e137344f50ec050267186b0e
Haoting Cheng <haoting3462@gmail.com>
2024-12-21 15:34:12 +0000

    feat: header

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,16 @@
-import Logo from "./logo";
+import Image from "next/image";
 import Menu from "./menu";
 
 const Header = () => {
-    return (
-        <header className="row-start-1 flex gap-6 flex-wrap items-start justify-between  ">
-            <div className="flex gap-2 p-2 lg:p-4"><Logo /> <span className="font-blacks">Turtle Spot Taiwan</span></div>
-            <Menu />
-        </header>
-    );
-}
+  return (
+    <header className="row-start-1 flex gap-6 flex-wrap items-start justify-between bg-ocean-light">
+      <div className="flex gap-4 p-2 lg:py-4 lg:px-6 font-black">
+        <Image src="/logo.svg" width={22} height={22} alt="logo" />
+        <span className="font-blacks text-2xl">Turtle Spot Taiwan</span>
+      </div>
+      <Menu />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
