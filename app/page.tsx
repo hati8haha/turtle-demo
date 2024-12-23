12a51021f51deff2df518a3a8795e9120da0f5d2
Haoting Cheng <haoting3462@gmail.com>
2024-12-23 00:14:24 +0000

    feat: add apollo client

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import CarouselSection from "@/sections/carousel";
 import FavoriteDiveSite from "@/sections/favoriteDiveSite";
 import Information from "@/sections/information";
+import WitnessStories from "@/sections/witnessStories";
 
 export default function Home() {
-	return (
-		<main className="flex flex-col items-center">
-			<Information />
-			<CarouselSection />
-			<FavoriteDiveSite />
-		</main>
-	);
+  return (
+    <main className="flex flex-col items-center">
+      <Information />
+      <CarouselSection />
+      <FavoriteDiveSite />
+      <WitnessStories />
+    </main>
+  );
 }
