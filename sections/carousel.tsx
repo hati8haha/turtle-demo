0d40888d0d7404d61d0a65d4aa34dab60391bcaf
Haoting Cheng <haoting3462@gmail.com>
2024-12-23 00:16:39 +0000

    chore: use section element for info section

--- sections/carousel.tsx
+++ sections/carousel.tsx
@@ -2,12 +2,12 @@ import Carousel from "@/components/carousel";
 
 const CarouselSection = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center bg-t-black-100 ">
+    <section className="w-full h-full flex flex-col items-center justify-center bg-t-black-100 ">
       <Carousel images={["/turtle.jpg", "/turtle2.jpg", "/turtle3.jpg"]} />
       <div className="bg-t-black-700 w-full h-10">
         <div className="bg-t-black-100 rounded-b-[40px] h-10 w-full" />
       </div>
-    </div>
+    </section>
   );
 };
 
